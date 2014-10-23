12732d5c450f4e344eaef5f3d0e04f54abba5c49
malkavia <malkavia@163.com>
2014-10-23 22:42:17 +0800

    Create New

---
+++ Homepwner/Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by Malkavia on 10/23/14.
+//  Copyright (c) 2014 Malkavia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/Homepwner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homepwner
+//
+//  Created by Malkavia on 10/23/14.
+//  Copyright (c) 2014 Malkavia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
